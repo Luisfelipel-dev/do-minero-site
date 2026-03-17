8b927dc43e275b1e856d6903defc972f6e5cb2dc
Luisfelipel-dev <luisfelipel@rocketmail.com>
2026-03-16 21:58:53 -0300

    atualizar

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const slides = document.querySelectorAll(".banner-slide");
+  let current = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+  }
+
+  window.nextBanner = function () {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  };
+
+  window.prevBanner = function () {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  };
+
+  showSlide(current);
+
+});
