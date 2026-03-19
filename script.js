65b391577774d39b909e7f980bf098153f35feb4
Luisfelipel-dev <luisfelipel@rocketmail.com>
2026-03-19 13:25:34 -0300

    atualizar

--- script.js
+++ script.js
@@ -22,15 +22,11 @@ current = (current - 1 + slides.length) % slides.length;
 showSlide(current);
 }
 
-/* =========================
-EVENTOS
-========================= */
+/* ====== EVENTOS======== */
 btnNext.addEventListener("click", nextBanner);
 btnPrev.addEventListener("click", prevBanner);
 
-/* =========================
-AUTOPLAY (PROFISSIONAL)
-========================= */
+/* ===== AUTOPLAY ======== */
 function startAutoPlay() {
 autoPlayInterval = setInterval(nextBanner, 4000); // troca a cada 4s
 }
@@ -45,9 +41,7 @@ const banner = document.querySelector(".banner-container");
 banner.addEventListener("mouseenter", stopAutoPlay);
 banner.addEventListener("mouseleave", startAutoPlay);
 
-/* =========================
-INICIALIZAÇÃO
-========================= */
+/* ===== INICIALIZAÇÃO ====== */
 showSlide(current);
 startAutoPlay();
 
